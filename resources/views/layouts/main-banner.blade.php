1771f29647a3379dcd762e102aa9c5e6dec82187
Sunny Singh <ishy.singh@gmail.com>
2018-12-17 12:12:15 -0600

    Added link to CTA button

--- resources/views/layouts/main-banner.blade.php
+++ resources/views/layouts/main-banner.blade.php
@@ -5,7 +5,7 @@
 		<div class="row fullscreen d-flex align-items-center justify-content-end">
 			<div class="banner-content col-lg-12 col-md-12">
 				<h1>We Help You <span>Build Brands</span> That Grow Your Business</h1>
-				<a href="#" class="primary-btn2 header-btn text-uppercase center-button">Hire Us Now!</a>
+				<a href="/services" class="primary-btn2 header-btn text-uppercase center-button">Hire Us Now!</a>
 			</div>
 		</div>
 	</div>
