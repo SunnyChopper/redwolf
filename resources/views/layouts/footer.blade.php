1c2d0933fe2f430c41764a633419432ec2fff50e
Sunny Singh <ishy.singh@gmail.com>
2018-12-23 18:13:08 -0600

    Updated site styles

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,31 +10,12 @@
 					</ul>
 				</div>
 			</div>
-			<div class="col-lg-6  col-md-12">
+			<div class="col-lg-6 col-md-12">
 				<div class="single-footer-widget newsletter">
-					<h6>Newsletter</h6>
-					<p>Get exclusive content, updates, and more delivered directly to your inbox.</p>
-					<div id="mc_embed_signup">
-						<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-
-							<div class="form-group row" style="width: 100%">
-								<div class="col-lg-8 col-md-12">
-									<input name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
-									<div style="position: absolute; left: -5000px;">
-										<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-									</div>
-								</div> 
-							
-								<div class="col-lg-4 col-md-12">
-									<button class="nw-btn primary-btn">Subscribe</button>
-								</div> 
-							</div>		
-							<div class="info"></div>
-						</form>
-					</div>		
+					
 				</div>
 			</div>
-			<div class="col-lg-3  col-md-12">
+			<div class="col-lg-3 col-md-12">
 				<div class="single-footer-widget">
 					<img src="{{ URL::asset('img/veteran-business.png') }}" class="regular-image-80 right-button mobile-left-button">
 				</div>
@@ -43,13 +24,11 @@
 
 		<div class="row d-flex justify-content-between">
 			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-<p class="col-lg-8 col-sm-12 footer-text m-0 text-white">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+<p class="col-lg-8 col-sm-12 footer-text pt-3 text-white">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 			<div class="col-lg-4 col-sm-12 footer-social">
-				<a href="#"><i class="fa fa-facebook"></i></a>
-				<a href="#"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-dribbble"></i></a>
-				<a href="#"><i class="fa fa-behance"></i></a>
+				<a href="https://www.facebook.com/Red-Wolf-Entertainment-2012759272379764/"><i class="fa fa-facebook"></i></a>
+				<a href="https://www.instagram.com/redwolfent/"><i class="fa fa-instagram"></i></a>
 			</div>
 		</div>
 	</div>
--- resources/views/layouts/main-banner.blade.php
+++ resources/views/layouts/main-banner.blade.php
@@ -2,7 +2,7 @@
 <section class="banner-area relative" id="home" data-parallax="scroll">
 	<div class="overlay-bg overlay"></div>
 	<div class="container">
-		<div class="row fullscreen d-flex align-items-center justify-content-end">
+		<div class="row front-page-banner d-flex align-items-center justify-content-end">
 			<div class="banner-content col-lg-12 col-md-12">
 				<h1>We Help You <span>Build Brands</span> That Grow Your Business</h1>
 				<a href="/services" class="primary-btn2 header-btn text-uppercase center-button">Learn More</a>
