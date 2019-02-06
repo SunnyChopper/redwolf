5cea7e6f0951b5ead76e6b06b20b1fb14e24647e
Singh, Sunny <isingh3@ilstu.edu>
2019-02-06 14:18:13 -0600

    Added more to portfolio

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -12,6 +12,50 @@
 
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/3WX20TMG7VA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/a0NEzcCYa1A?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/4l4brM4-wEs?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/wqrt85adXxQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/1ny66M9bdVQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/cRTPeelSwDk?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16">
+				<div class="videoWrapper">
+				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/QgXVs469sFA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				</div>
+			</div>
+		</div>
 	</div>
 
 	@include('layouts.cta-row')
