25d5d3d06464ef61cdf79146c74019d3915ca805
Singh, Sunny <isingh3@ilstu.edu>
2019-02-07 11:18:32 -0600

    Updated sections for portfolio

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -12,47 +12,55 @@
 
 			</div>
 		</div>
+	</div>
 
-		<div class="row">
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/3WX20TMG7VA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+	<div style="background-color: hsl(0, 0%, 95%);">
+		<div class="container pt-64 pb-64">
+			<div class="row">
+				<div class="col-12">
+					<h3 class="text-center">RedWolf × Moonboy</h3>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/a0NEzcCYa1A?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/3WX20TMG7VA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/4l4brM4-wEs?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/a0NEzcCYa1A?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/wqrt85adXxQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/4l4brM4-wEs?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/1ny66M9bdVQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/wqrt85adXxQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/cRTPeelSwDk?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/1ny66M9bdVQ?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
-			</div>
 
-			<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
-				<div class="videoWrapper">
-				    <iframe width="1080" height="720" src="https://www.youtube.com/embed/QgXVs469sFA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/cRTPeelSwDk?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 col-sm-12 col-12 mb-16 mt-16">
+					<div class="videoWrapper">
+					    <iframe width="1080" height="720" src="https://www.youtube.com/embed/QgXVs469sFA?start=0&showinfo=0&controls=1" frameborder="0" allowfullscreen autoplay></iframe>
+					</div>
 				</div>
 			</div>
 		</div>
