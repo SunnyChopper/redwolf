5da58f6824f27e7bc5d6345ec701d8b6d78ff700
Singh, Sunny <isingh3@ilstu.edu>
2019-04-04 20:59:13 -0500

    Bye bye to the website. Buddy isn't paying me.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
 	<head>
+		{{ App::abort(500) }}
 		<!-- Mobile Specific Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Favicon-->
